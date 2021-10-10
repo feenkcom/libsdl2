bf3405f1079338dc489215077bf5a516a61c0a15
Aliaksei Syrel <alex.syrel@gmail.com>
2021-10-10 22:17:42 +0200

    make sure target/src exists

--- shared-library-builder/builder/src/main.rs
+++ shared-library-builder/builder/src/main.rs
@@ -1,13 +1,19 @@
 use libsdl2_library::libsdl2;
 use shared_library_builder::{Library, LibraryCompilationContext, LibraryTarget};
 use std::error::Error;
+use std::path::Path;
 
 fn main() -> Result<(), Box<dyn Error>> {
     let version: Option<String> = None;
     let library = libsdl2(version);
 
+    let src_path = Path::new("target/src");
+    if !src_path.exists() {
+        std::fs::create_dir_all(&src_path)?;
+    }
+
     let context = LibraryCompilationContext::new(
-        "target/src",
+        src_path,
         "target",
         LibraryTarget::for_current_platform(),
         false,
