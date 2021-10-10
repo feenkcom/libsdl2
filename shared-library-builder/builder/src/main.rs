16a6d1430bf98facf195f4e84ee7d17ba98f5417
Aliaksei Syrel <alex.syrel@gmail.com>
2021-10-10 22:00:22 +0200

    build sdl2 under target build dir

--- shared-library-builder/builder/src/main.rs
+++ shared-library-builder/builder/src/main.rs
@@ -1,12 +1,17 @@
 use libsdl2_library::libsdl2;
-use shared_library_builder::{Library, LibraryCompilationContext};
+use shared_library_builder::{Library, LibraryCompilationContext, LibraryTarget};
 use std::error::Error;
 
 fn main() -> Result<(), Box<dyn Error>> {
     let version: Option<String> = None;
     let library = libsdl2(version);
 
-    let context = LibraryCompilationContext::new_release("target");
+    let context = LibraryCompilationContext::new(
+        "target",
+        "target",
+        LibraryTarget::for_current_platform(),
+        false,
+    );
     let compiled_library = library.compile(&context)?;
     println!("Compiled {}", compiled_library.display());
     Ok(())
